14cd949fa6d3971731ba47a151aca8f9ca91209d
Stephen J Surtees <stephen.publicmail@gmail.com>
2020-01-30 21:12:11 +0000

    Small recursion tree demo

--- src/App.js
+++ src/App.js
@@ -2,25 +2,48 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function mkNode(name, children) {
+    console.log("created node, ", name);
+    return ({
+        name: name,
+        children: children
+    });
+};
+
+const data = mkNode("one", [
+    mkNode("twoA", null),
+    mkNode("twoB", null),
+    mkNode("twoC",
+    [
+        mkNode("threeB", null),
+        mkNode("threeA", [
+            mkNode("go deep", [
+                mkNode("more right", [
+                    mkNode("gotta love recursion", null),
+                ]),
+                mkNode("fun", null),
+            ]),
+            mkNode("another leaf 1", null),
+            mkNode("another leaf 2", null),
+            mkNode("another leaf 3", null),
+        ]),
+    ])
+]);
+
+function showNodes(node) {
+    if (!node.children) {
+        return <li>{node.name}</li>
+    }
+    let list = [];
+    for (let i=0; i<node.children.length; i++) {
+        list.push(showNodes(node.children[i]));
+    }
+    return (<div><h3>{node.name}</h3><ul>{list}</ul></div>);
+}
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return showNodes(data);
 }
 
 export default App;
